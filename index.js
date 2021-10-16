975be2c531868ddde249e94047deb7408e50329c
Punhan44 <87721566+Punhan44@users.noreply.github.com>
2021-10-16 20:02:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,105 @@ medianav.addEventListener("click" , function (){
   mediasidebar.classList.toggle("show-sidebar")
   colmd.classList.toggle("col-lg-9-open")
 })
+
+
+
+var sliderPictures=[
+  {
+    image:"images/mekteb.jpeg"
+  },
+  {
+    image:"images/mekteb3.jpeg"
+  },
+
+  {
+    image:"images/mekteb5.jpeg"
+  },
+  {
+    image:"images/mekteb6.jpeg"
+  },
+  {
+    image:"images/mekteb7.jpeg"
+  },
+  {
+    image:"images/mekteb8.jpeg"
+  }
+
+  
+];
+var index=0;
+var slaytCount=sliderPictures.length;
+var interval;
+
+var settings={
+  duration:'6000',
+  random:false
+}
+
+init(settings)
+
+
+
+
+document.querySelector(".fa-arrow-left").addEventListener('click',function(){
+index --;
+showSlide(index);
+console.log(index )
+});
+
+document.querySelector(".fa-arrow-right").addEventListener('click',function(){
+  index ++;
+  showSlide(index);
+console.log(index);
+});
+
+document.querySelectorAll(".arrow").forEach(function(item){
+  item.addEventListener('mouseenter',function(){
+    clearInterval(interval);
+  })
+})
+document.querySelectorAll(".arrow").forEach(function(item){
+  item.addEventListener('mouseleave',function(){
+   init(settings);
+  })
+})
+
+
+
+
+function init(settings){
+  var prev;
+interval=setInterval(function(){
+   if(settings.random){
+     do{
+       index = Math.floor(Math.random() * slaytCount);
+     }while(index == prev)
+     prev = index;
+   }else{
+     if(slaytCount == index -1){
+       index = 0;
+     }
+     showSlide(index);
+     
+     index++;
+   }
+
+ 
+  showSlide(index)
+
+},settings.duration)
+}
+
+
+function showSlide(i){
+  index=i;
+  if(i<0){
+    index=slaytCount-1;
+  }
+  if(i>=slaytCount){
+    index=0;  
+  }
+  document.querySelector(".slider-image").setAttribute('src',sliderPictures[index].image)
+}
+
+
